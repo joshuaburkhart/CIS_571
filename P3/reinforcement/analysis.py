e424f6f3c9ae39c1d6683f321db29acc72c706f0
Joshua Burkhart <burkhart.joshua@gmail.com>
2012-11-17 13:28:31 -0800

    question 3 ..

--- P3/reinforcement/analysis.py
+++ P3/reinforcement/analysis.py
@@ -15,41 +15,37 @@
 
 def question2():
   answerDiscount = 0.9
-  answerNoise = 0.2
+  answerNoise = 0.0
   return answerDiscount, answerNoise
 
 def question3a():
   answerDiscount = 0.9
   answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerLivingReward = -3.0
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-  answerDiscount = 0.9
-  answerNoise = 0.2
-  answerLivingReward = 0.0
-  return answerDiscount, answerNoise, answerLivingReward
-  # If not possible, return 'NOT POSSIBLE'
+  return 'NOT POSSIBLE'
 
 def question3c():
-  answerDiscount = 0.9
+  answerDiscount = 0.99
   answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerLivingReward = -2.0
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-  answerDiscount = 0.9
+  answerDiscount = 0.99
   answerNoise = 0.2
   answerLivingReward = 0.0
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-  answerDiscount = 0.9
+  answerDiscount = 0.99
   answerNoise = 0.2
-  answerLivingReward = 0.0
+  answerLivingReward = 1.0
   return answerDiscount, answerNoise, answerLivingReward
   # If not possible, return 'NOT POSSIBLE'
 
